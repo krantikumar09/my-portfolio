92afbe0a55a8ded924dad9dac6d0d991f586c733
Krantikumar Mathapati <krantikumar3446@gmail.com>
2024-10-26 19:44:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,5 +23,5 @@ app.use("/api/project", projectRoute);
 
 // start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on: http://localhost:${PORT}`);
+  console.log(`Server is running...`);
 });
